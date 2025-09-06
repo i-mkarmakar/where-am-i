cfe39ac7de9a90d3e2291ecc4cd8d554bfba8a18
Manish Karmakar <mrinalkarmakar565@gmail.com>
2025-09-06 17:15:33 +0530

    Refactor socket.io setup and improve connection handling

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const http = require("http");
 const socketIo = require("socket.io");
 
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+  cors: { origin: "*" },
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -13,11 +15,14 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 io.on("connection", (socket) => {
-  socket.on("send-location", function (data) {
+  console.log("New client connected:", socket.id);
+
+  socket.on("send-location", (data) => {
     io.emit("receive-location", { id: socket.id, ...data });
   });
-  socket.on("disconnect", function () {
-    io.emit("user-disconnect", socket.id);
+
+  socket.on("disconnect", () => {
+    io.emit("user-disconnected", socket.id);
   });
 });
 
@@ -25,6 +30,7 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-server.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
